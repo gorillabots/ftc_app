b219ba4293949fbcbc385313a9df30ddde3dae52
Jarred Moyer <jarredds941@gmail.com>
2015-11-29 12:25:57 -0500

    cleaned up the register

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,21 +55,21 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
 
-    manager.register("NullOp", NullOp.class);
-manager.register("armTest" , armTest.class);
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
-    manager.register ("RemoteControl", RemoteControl.class);
-manager.register("CompTele", CompTele.class);
-      manager.register("teletest1", teletest1.class);
-      manager.register("teletest2", teletest2.class);
-      manager.register("teletest3", teletest3.class);
-      manager.register("teletest1half", teletest1half.class);
-      manager.register("teletest2half", teletest2half.class);
+      //manager.register("NullOp", NullOp.class);
+      //manager.register("armTest" , armTest.class);
+      //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+      //manager.register("K9TeleOp", K9TeleOp.class);
+      //manager.register ("PushBotAuto", PushBotAuto.class);
+      //manager.register ("PushBotManual", PushBotManual.class);
+      //manager.register ("RemoteControl", RemoteControl.class);
+      manager.register("CompTele", CompTele.class);
+     //manager.register("teletest1", teletest1.class);
+      //manager.register("teletest2", teletest2.class);
+      //manager.register("teletest3", teletest3.class);
+      //manager.register("teletest1half", teletest1half.class);
+      //manager.register("teletest2half", teletest2half.class);
       manager.register("mountTest", mountTest.class);
-      manager.register("JoyStickMountTest", JoyStickMountTest.class);
+      //manager.register("JoyStickMountTest", JoyStickMountTest.class);
       manager.register("CorkscrewTest", CorkscrewTest.class);
       manager.register("PresserTester", PresserTester.class);
       manager.register("JarredServoTest",JarredServoTest.class);
