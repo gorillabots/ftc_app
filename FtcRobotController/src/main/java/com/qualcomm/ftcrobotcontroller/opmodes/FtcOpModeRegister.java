7157c79f120e48341f14f896ac845b4779239e1b
Michelle <nishimototama@gmail.com>
2015-12-04 20:08:44 -0500

    Registered self tets

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -79,7 +79,9 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("TESTTTTTTTT",servotestPos.class);
     manager.register("TESTclose",closeTest.class);
     manager.register("15sec.test",fifteensecs.class);
-    /*
+    manager.register("Robot self-test",Comp_Tst.class);
+
+     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
 
