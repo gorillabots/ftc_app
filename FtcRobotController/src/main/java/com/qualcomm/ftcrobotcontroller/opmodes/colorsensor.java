06bb36c724e6e57d146b6aed930337e9d450649b
Chris Soper <ChrisSoper1@gmail.com>
2016-02-21 14:38:36 -0500

    created get color function defines red, blue, red line, blue line

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/colorsensor.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/colorsensor.java
@@ -11,30 +11,48 @@ public class colorsensor extends OpMode{
     public void init() {
     color = hardwareMap.colorSensor.get("color_sensor");
     }
+
+    public String getColor(){
+        String currentcolor = "none";
+
+        if (color.red()>color.blue() && color.red()>color.green() && color.green()== color.blue()){
+
+            currentcolor = "red";
+        }
+
+        if (color.red()<color.blue() && color.green()<color.blue()){
+
+            currentcolor = "blue";
+        }
+
+        if (color.red()>color.green() && color.red()>color.blue() && color.green()>color.blue()){
+
+            currentcolor = "red line";
+        }
+
+        if (color.red()<color.green() && color.blue()<color.green() && color.red() == color.blue()){
+
+            currentcolor = "blue line";
+        }
+        telemetry.addData("current_color" , currentcolor);
+        return currentcolor;
+    }
     public void loop() {
         color.enableLed(false);
         telemetry.addData("red value", color.red());
         telemetry.addData("blue value", color.blue());
-        if (color.red()>color.blue() && color.red()>color.green() ){
-            telemetry.addData("current_color", "Red");
-        }
-        if (color.red()<color.blue()){
-            telemetry.addData("current_color", "Blue");
-        }
-        if (color.red()>color.green() && color.red()>color.blue()){
-            telemetry.addData("current_color", "red line");;
-        }
-        if (color.red()<color.alpha()){
-            telemetry.addData("current_color", "blue line");
-        }
+        String whatColorAmILookingAt = getColor();
+
 //        if (color.alpha()>color.blue() && color.alpha()>color.green() && color.alpha()>color.red()){
 //            telemetry.addData("current_color", "white line");
+
+//        if (color.blue()<color.red() && color.red()<color.green() && color.green()>color.blue())
 //        }
 
         telemetry.addData("red value", color.red());
         telemetry.addData("blue value", color.blue());
         telemetry.addData("green value", color.green());
-        telemetry.addData("white value", color.alpha());
+//        telemetry.addData("white value", color.alpha());
 
 }
     public void stop() {
